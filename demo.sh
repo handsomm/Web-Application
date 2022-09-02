ee17a8f42c418284b5a43da5d0d7cf7e6ae4a264
Shibu Handsomm <soumyaranjanandcbu@gmail.com>
2022-09-02 08:59:43 +0530

    1st commit

---
+++ demo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -d .git ];
+then
+	echo "Git already initialized"
+else
+	git init
+	echo "Enter the URL"
+	read url
+	git remote add origin $url
+fi
+
+git add .
+echo "Enter commit message"
+read msg
+git commit -m "$msg"
+#txt="nothing to commit, working tree clean"
+
+#var=$(git status | awk "NR==5 {print $1}")
+
+#if [ "$var" = "$txt" ];
+#then
+#	echo "Hello"
+#else
+#	echo "Hyyy"
+#fi
